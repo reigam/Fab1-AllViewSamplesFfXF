ea4c7f29e58f42b58961a27f8cd9b14fb0a3aff2
Reigam <rbonnemann@live.de>
2021-04-13 23:01:25 +0200

    present-path done

--- AllViewSamplesFfXF/Present.fs
+++ AllViewSamplesFfXF/Present.fs
@@ -19,194 +19,197 @@ module SamplePresent =
                             (
                                 horizontalOptions = style.Position,
                                 verticalOptions = style.Position,
-                                backgroundColor = style.ViewColor
+                                color = Color.Black
                             )
                 )
         }
-        //{   Name = "Sample";  
+        {   Name = "Image";  
+            SampleType = "basic";
+            Page = 
+                View.ContentPage(                    
+                    backgroundColor = style.PageColor,
+                    title ="Image",                         
+                    content = 
+                        View.Image
+                            (
+                                horizontalOptions = style.Position,
+                                verticalOptions = style.Position,
+                                backgroundColor = style.ViewColor,
+                                source = Image.ImagePath "icon.png"
+                            )
+                )
+        }
+        {   Name = "Label";  
+            SampleType = "basic";
+            Page = 
+                View.ContentPage(                    
+                    backgroundColor = style.PageColor,
+                    title ="Label",                         
+                    content = 
+                        View.Label
+                            (
+                                horizontalOptions = style.Position,
+                                verticalOptions = style.Position,
+                                backgroundColor = style.ViewColor,
+                                padding = style.Padding,
+                                text = "Label"
+                            )
+                )
+        }
+        //{   Name = "Map";  
         //    SampleType = "basic";
         //    Page = 
         //        View.ContentPage(                    
         //            backgroundColor = style.PageColor,
-        //            title ="Sample",                         
+        //            title ="Map",                         
         //            content = 
-        //                View.Sample
+        //                View.Map
         //                    (
         //                        horizontalOptions = style.Position,
         //                        verticalOptions = style.Position,
         //                        backgroundColor = style.ViewColor,
         //                        padding = style.Padding,
-        //                        text = "Sample"
+        //                        text = "Map"
         //                    )
         //        )
         //}
-        //{   Name = "Sample";  
+        {   Name = "Ellipse";  
+            SampleType = "basic";
+            Page = 
+                View.ContentPage(                    
+                    backgroundColor = style.PageColor,
+                    title ="Ellipse",                         
+                    content = 
+                        View.Ellipse
+                            (
+                                horizontalOptions = style.Position,
+                                verticalOptions = style.Position,
+                                backgroundColor = style.ViewColor,
+                                fill = View.SolidColorBrush(Color.Black),
+                                width = 50.,
+                                height = 50.
+                            )
+                )
+        }
+        {   Name = "Line";  
+            SampleType = "basic";
+            Page = 
+                View.ContentPage(                    
+                    backgroundColor = style.PageColor,
+                    title ="Line",                         
+                    content = 
+                        View.Line
+                            (
+                                horizontalOptions = style.Position,
+                                verticalOptions = style.Position,
+                                backgroundColor = style.ViewColor,
+                                stroke = View.SolidColorBrush(Color.Black),
+                                x1 = 40., 
+                                y1 = 0., 
+                                x2 = 0., 
+                                y2 = 120.
+                            )
+                )
+        }
+        {   Name = "Path";  
+            SampleType = "basic";
+            Page = 
+                View.ContentPage(                    
+                    backgroundColor = style.PageColor,
+                    title ="Path",                         
+                    content = 
+                        View.Path
+                            (
+                                horizontalOptions = style.Position,
+                                verticalOptions = style.Position,
+                                backgroundColor = style.ViewColor,                                
+                                stroke = View.SolidColorBrush(Color.Black),
+                                data = Content.fromString "M 10,100 C 100,0 200,200 300,100"
+                            )
+                )
+        }
+        //{   Name = "Sample2";  
         //    SampleType = "basic";
         //    Page = 
         //        View.ContentPage(                    
         //            backgroundColor = style.PageColor,
-        //            title ="Sample",                         
+        //            title ="Sample2",                         
         //            content = 
-        //                View.Sample
+        //                View.Sample2
         //                    (
         //                        horizontalOptions = style.Position,
         //                        verticalOptions = style.Position,
         //                        backgroundColor = style.ViewColor,
         //                        padding = style.Padding,
-        //                        text = "Sample"
+        //                        text = "Sample2"
         //                    )
         //        )
         //}
-        //{   Name = "Sample";  
+        //{   Name = "Sample2";  
         //    SampleType = "basic";
         //    Page = 
         //        View.ContentPage(                    
         //            backgroundColor = style.PageColor,
-        //            title ="Sample",                         
+        //            title ="Sample2",                         
         //            content = 
-        //                View.Sample
+        //                View.Sample2
         //                    (
         //                        horizontalOptions = style.Position,
         //                        verticalOptions = style.Position,
         //                        backgroundColor = style.ViewColor,
         //                        padding = style.Padding,
-        //                        text = "Sample"
+        //                        text = "Sample2"
         //                    )
         //        )
         //}
-        //{   Name = "Sample";  
+        //{   Name = "Sample2";  
         //    SampleType = "basic";
         //    Page = 
         //        View.ContentPage(                    
         //            backgroundColor = style.PageColor,
-        //            title ="Sample",                         
+        //            title ="Sample2",                         
         //            content = 
-        //                View.Sample
+        //                View.Sample2
         //                    (
         //                        horizontalOptions = style.Position,
         //                        verticalOptions = style.Position,
         //                        backgroundColor = style.ViewColor,
         //                        padding = style.Padding,
-        //                        text = "Sample"
+        //                        text = "Sample2"
         //                    )
         //        )
         //}
-        //{   Name = "Sample";  
+        //{   Name = "Sample2";  
         //    SampleType = "basic";
         //    Page = 
         //        View.ContentPage(                    
         //            backgroundColor = style.PageColor,
-        //            title ="Sample",                         
+        //            title ="Sample2",                         
         //            content = 
-        //                View.Sample
+        //                View.Sample2
         //                    (
         //                        horizontalOptions = style.Position,
         //                        verticalOptions = style.Position,
         //                        backgroundColor = style.ViewColor,
         //                        padding = style.Padding,
-        //                        text = "Sample"
+        //                        text = "Sample2"
         //                    )
         //        )
         //}
-        //{   Name = "Sample";  
+        //{   Name = "Sample2";  
         //    SampleType = "basic";
         //    Page = 
         //        View.ContentPage(                    
         //            backgroundColor = style.PageColor,
-        //            title ="Sample",                         
+        //            title ="Sample2",                         
         //            content = 
-        //                View.Sample
+        //                View.Sample2
         //                    (
         //                        horizontalOptions = style.Position,
         //                        verticalOptions = style.Position,
         //                        backgroundColor = style.ViewColor,
         //                        padding = style.Padding,
-        //                        text = "Sample"
-        //                    )
-        //        )
-        //}
-        //{   Name = "Sample";  
-        //    SampleType = "basic";
-        //    Page = 
-        //        View.ContentPage(                    
-        //            backgroundColor = style.PageColor,
-        //            title ="Sample",                         
-        //            content = 
-        //                View.Sample
-        //                    (
-        //                        horizontalOptions = style.Position,
-        //                        verticalOptions = style.Position,
-        //                        backgroundColor = style.ViewColor,
-        //                        padding = style.Padding,
-        //                        text = "Sample"
-        //                    )
-        //        )
-        //}
-        //{   Name = "Sample";  
-        //    SampleType = "basic";
-        //    Page = 
-        //        View.ContentPage(                    
-        //            backgroundColor = style.PageColor,
-        //            title ="Sample",                         
-        //            content = 
-        //                View.Sample
-        //                    (
-        //                        horizontalOptions = style.Position,
-        //                        verticalOptions = style.Position,
-        //                        backgroundColor = style.ViewColor,
-        //                        padding = style.Padding,
-        //                        text = "Sample"
-        //                    )
-        //        )
-        //}
-        //{   Name = "Sample";  
-        //    SampleType = "basic";
-        //    Page = 
-        //        View.ContentPage(                    
-        //            backgroundColor = style.PageColor,
-        //            title ="Sample",                         
-        //            content = 
-        //                View.Sample
-        //                    (
-        //                        horizontalOptions = style.Position,
-        //                        verticalOptions = style.Position,
-        //                        backgroundColor = style.ViewColor,
-        //                        padding = style.Padding,
-        //                        text = "Sample"
-        //                    )
-        //        )
-        //}
-        //{   Name = "Sample";  
-        //    SampleType = "basic";
-        //    Page = 
-        //        View.ContentPage(                    
-        //            backgroundColor = style.PageColor,
-        //            title ="Sample",                         
-        //            content = 
-        //                View.Sample
-        //                    (
-        //                        horizontalOptions = style.Position,
-        //                        verticalOptions = style.Position,
-        //                        backgroundColor = style.ViewColor,
-        //                        padding = style.Padding,
-        //                        text = "Sample"
-        //                    )
-        //        )
-        //}
-        //{   Name = "Sample";  
-        //    SampleType = "basic";
-        //    Page = 
-        //        View.ContentPage(                    
-        //            backgroundColor = style.PageColor,
-        //            title ="Sample",                         
-        //            content = 
-        //                View.Sample
-        //                    (
-        //                        horizontalOptions = style.Position,
-        //                        verticalOptions = style.Position,
-        //                        backgroundColor = style.ViewColor,
-        //                        padding = style.Padding,
-        //                        text = "Sample"
+        //                        text = "Sample2"
         //                    )
         //        )
         //}
